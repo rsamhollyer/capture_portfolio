7887682f9111c095de34cd95ed4362f16fd2f239
rsamhollyer <rsamhollyer@gmail.com>
2021-04-05 18:55:04 -0500

    add services section comp

---
+++ src/components/ServicesSection.jsx
@@ -0,0 +1,58 @@
+import clock from "../assets/images/clock.svg";
+import diaphragm from "../assets/images/diaphragm.svg";
+import money from "../assets/images/money.svg";
+import teamwork from "../assets/images/teamwork.svg";
+import home2 from "../assets/images/home2.png";
+
+function ServicesSection() {
+  return (
+    <div className="services">
+      <div className="description">
+        <h2>
+          High <span>quality</span> services
+        </h2>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={clock} alt="clock icon" />
+              <h3>Efficient</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="teamwork icon" />
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={diaphragm} alt="diaphragm icon" />
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="money icon" />
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+      <div className="image">
+        <img src={home2} alt="a camera" />
+      </div>
+    </div>
+  );
+}
+
+export default ServicesSection;
