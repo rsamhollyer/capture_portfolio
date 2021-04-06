1320bdb6973a863b3d6b1f3f2d3798e26233a48d
rsamhollyer <rsamhollyer@gmail.com>
2021-04-06 10:06:52 -0500

    add to our work comp and styles

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -1,8 +1,38 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Work, Movie } from "../styles";
+import athlete from "../assets/images/athlete-small.png";
+import theracer from "../assets/images/theracer-small.png";
+import goodtimes from "../assets/images/goodtimes-small.png";
+
 function OurWork() {
   return (
-    <div>
-      <h1>Our Work</h1>
-    </div>
+    <Work>
+      <Movie>
+        <h2>The Athelete</h2>
+
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="boxer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="a car" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="a couple" />
+        </Link>
+      </Movie>
+    </Work>
   );
 }
 
--- src/styles.jsx
+++ src/styles.jsx
@@ -79,3 +79,26 @@ export const StyledNav = styled.nav`
     position: relative;
   }
 `;
+
+export const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0;
+  }
+`;
+
+export const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #ccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
