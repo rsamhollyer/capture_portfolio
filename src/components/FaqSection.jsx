1523b6df5512378da3deb4c6c3f2a7d17929775d
rsamhollyer <rsamhollyer@gmail.com>
2021-04-06 09:21:39 -0500

    add more styled

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,6 +1,9 @@
+import styled from "styled-components";
+import { About } from "../styles";
+
 function FaqSection() {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -13,6 +16,7 @@ function FaqSection() {
             Consequuntur, possimus!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>Daily Schedule</h4>
@@ -23,6 +27,7 @@ function FaqSection() {
             Consequuntur, possimus!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>differnt Payment Methods?</h4>
@@ -33,6 +38,7 @@ function FaqSection() {
             Consequuntur, possimus!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="question">
         <h4>What Product do You Offer?</h4>
@@ -43,9 +49,41 @@ function FaqSection() {
             Consequuntur, possimus!
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 }
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0;
+    cursor: pointer;
+  }
+
+  .answer {
+    padding: 3rem 0;
+    p {
+      padding: 1rem 0;
+    }
+  }
+`;
 
 export default FaqSection;
--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -45,6 +45,7 @@ h3 {
 
 h4 {
     font-weight: bold;
+    font-size:2rem;
 }
 
 p {
